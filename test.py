5facc0f59690cea9ce989df5bc5047a20fc763a9
Chriswsd <kniimura.ping.pong@icloud.com>
2020-08-17 13:35:59 +0900

    coding test

---
+++ test.py
@@ -0,0 +1,28 @@
+
+def main():
+    l = []
+    n = 0
+    while True:
+        s = input()
+        if ':' not in s:
+            n = int(s)
+            break
+        else:
+            a, b = s.split(':')
+            a = int(a)
+            l.append((a, b))
+    
+    l.sort()
+    ans = ""
+
+    for e in l:
+        if n % e[0] == 0:
+            ans += e[1]
+
+    if ans == "":
+        print(n)
+    else:
+        print(ans)
+
+if __name__ == "__main__":
+    main()
